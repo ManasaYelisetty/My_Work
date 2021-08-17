693cffd23ea67cf27067b2fdfa01faba81286b21
ManasaYelisetty <87935671+ManasaYelisetty@users.noreply.github.com>
2021-08-17 13:57:14 +0530

    Add files via upload

---
+++ springbootwithspringmvc/springbootwithspringmvc/src/main/java/com/springbootwithspringmvc/SpringbootwithspringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.springbootwithspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithspringmvcApplication.class, args);
+	}
+
+}
---
+++ springbootwithspringmvc/springbootwithspringmvc/src/test/java/com/springbootwithspringmvc/SpringbootwithspringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootwithspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithspringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
