8f461d116a309530f5de7dae40dd23ff8e7184ad
ManasaYelisetty <87935671+ManasaYelisetty@users.noreply.github.com>
2021-08-17 12:25:16 +0530

    Add files via upload

---
+++ DataBaseConnectionDemo/src/main/webapp/dao/RegisterDaoIntf.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import java.util.List;
+
+
+import com.model.Register;
+
+public interface RegisterDaoIntf {
+
+	void saveRegister(Register register);
+
+	List<Register> getUsers();
+
+}
