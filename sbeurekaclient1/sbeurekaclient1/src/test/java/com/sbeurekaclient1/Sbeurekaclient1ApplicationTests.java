ccb3b414782125d4b1af708e3fe3ade4efba419d
ManasaYelisetty <87935671+ManasaYelisetty@users.noreply.github.com>
2021-08-18 12:16:30 +0530

    Add files via upload

---
+++ sbeurekaclient1/sbeurekaclient1/src/test/java/com/sbeurekaclient1/Sbeurekaclient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbeurekaclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbeurekaclient1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
