90944acfe877d8b436a5bc828d73135ac21a796a
ManasaYelisetty <87935671+ManasaYelisetty@users.noreply.github.com>
2021-08-23 11:58:51 +0530

    Delete SpringBootAngular/SpringBootAngular directory

--- SpringBootAngular/SpringBootAngular/src/main/java/com/SpringBootAngular/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBootAngular.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SpringBootAngular.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer,Integer> {
-
-}
