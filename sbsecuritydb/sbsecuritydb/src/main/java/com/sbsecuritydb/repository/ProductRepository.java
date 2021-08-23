5f66521751040d194fbe70376ad682b6892257d2
ManasaYelisetty <87935671+ManasaYelisetty@users.noreply.github.com>
2021-08-23 12:11:28 +0530

    Delete sbsecuritydb/sbsecuritydb directory

--- sbsecuritydb/sbsecuritydb/src/main/java/com/sbsecuritydb/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbsecuritydb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sbsecuritydb.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
